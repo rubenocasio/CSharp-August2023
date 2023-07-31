c9cfa300fb31388e22ab72cecdf667ee72a08949
Ruben Ocasio <rocasio@rubenocasio.com>
2023-07-31 14:49:07 -0400

    !

--- Week1/w1d1/FirstCSharp/Program.cs
+++ Week1/w1d1/FirstCSharp/Program.cs
@@ -1,6 +1,6 @@
 ﻿// See https://aka.ms/new-console-template for more information
 Console.WriteLine("Hello, World!!");
-
+//dotnet new console -f net6.0 -o FirstCSharp
 
 // let myName = "Ruben Ocasio";
 // console.log(myName); //Ruben Ocasio
@@ -10,6 +10,7 @@ Console.WriteLine(myName);
 
 char myName2 = 'A';
 Console.WriteLine(myName2);
+
 int myNum = 7;
 
 double myDec = 09.25;
