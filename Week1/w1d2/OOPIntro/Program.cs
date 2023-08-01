b7a4ef7494a377362709b7809cff08fe6c892cb0
Ruben Ocasio <rocasio@rubenocasio.com>
2023-08-01 16:56:00 -0400

    Update Program.cs

--- Week1/w1d2/OOPIntro/Program.cs
+++ Week1/w1d2/OOPIntro/Program.cs
@@ -4,13 +4,37 @@
 
 
 Person personOne = new Person("Kurt", "Clausen");
+Person personTwo = new Person("Ben", "Gomez");
+Person personThree = new Person("James", "Watson");
 
 // Console.WriteLine(personOne.FirstName);
 // Console.WriteLine(personOne.LastName);
-Console.WriteLine(personOne.FullName());
+// Console.WriteLine(personOne.FullName());
 
-Student studentOne = new Student("Aaron", "Hsu", "C#", 0925);
+Student studentOne = new Student("Aaron", "Hsu", "C#", 1039);
+Student studentTwo = new Student("Aleksey", "Kashubin", "C#", 0552);
+Student studentThree = new Student("Nicholas", "Tangsouvanh", "C#", 6754);
 
-Console.WriteLine(studentOne.FullName());
-Console.WriteLine(studentOne.CurrentStack);
-Console.WriteLine(studentOne.StudentId);
+// Console.WriteLine(studentOne.FullName());
+// Console.WriteLine(studentOne.CurrentStack);
+// Console.WriteLine(studentOne.StudentId);
+
+// List<Person> personList = new List<Person>()
+// {
+//     personOne,
+//     personTwo,
+//     personThree
+// };
+// foreach(Person p in personList)
+// {
+//     Console.WriteLine(p.FullName());
+// }
+
+
+List<Student> studentList = new List<Student>();
+studentList.Add(studentOne);
+studentList.Add(studentTwo);
+studentList.Add(studentThree);
+
+Lecture myLecture = new Lecture("C#: OOP", 4, personOne, studentList);
+myLecture.PrintAttendance();
