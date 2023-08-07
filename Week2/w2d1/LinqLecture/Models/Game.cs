8661c507eb423c12731967fba69051888a11e3b3
Ruben Ocasio <rocasio@rubenocasio.com>
2023-08-07 16:59:09 -0400

    Update Game.cs

--- Week2/w2d1/LinqLecture/Models/Game.cs
+++ Week2/w2d1/LinqLecture/Models/Game.cs
@@ -1,1 +1,13 @@
-#pragma 
+#pragma warning disable CS8618
+using System.Runtime.CompilerServices;
+
+namespace LinqLecture.Models;
+
+public class Game
+{
+    public string Title {get; set;}
+    public double Price {get; set;}
+    public string Genre {get; set;}
+    public string Rating {get; set;}
+    public string Platform {get; set;}
+}
